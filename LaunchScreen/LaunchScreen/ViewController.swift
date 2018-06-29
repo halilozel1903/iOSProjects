c171be23473007cd6516edaf31d60b6803195182
Halil Özel <halilozel1903@gmail.com>
2018-06-29 22:32:59 +0300

    Launch Screen projesi eklendi.

---
+++ LaunchScreen/LaunchScreen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LaunchScreen
+//
+//  Created by Halil Özel on 29.06.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
