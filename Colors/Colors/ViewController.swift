247c6bbda140a02b0fe8b0809143e75a4dab0ac3
Halil Özel <halilozel1903@gmail.com>
2018-06-29 22:31:45 +0300

    Colors dersi eklendi.

---
+++ Colors/Colors/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Colors
+//
+//  Created by Halil Özel on 29.06.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    @IBAction func greenClicked(_ sender: Any) {
+        view.backgroundColor = UIColor.green // arka plan rengini yeşil yaptık.
+    }
+    
+    
+    @IBAction func yellowClicked(_ sender: Any) {
+        view.backgroundColor = UIColor.yellow // arka plan rengini sarı yaptık.
+    }
+    
+    
+    @IBAction func redClicked(_ sender: Any) {
+        view.backgroundColor = UIColor.red // arka plan rengini kırmızı yaptık.
+    }
+    
+    @IBAction func blueClicked(_ sender: Any) {
+        view.backgroundColor = UIColor.blue // arka plan rengini mavi yaptık.
+    }
+    
+    
+    @IBAction func orangeClicked(_ sender: Any) {
+        view.backgroundColor = UIColor.orange // arka plan rengini turuncu yaptık.
+    }
+    
+    @IBAction func grayClicked(_ sender: Any) {
+        view.backgroundColor = UIColor.gray // arka plan rengini gri yaptık.
+    }
+    
+}
+
