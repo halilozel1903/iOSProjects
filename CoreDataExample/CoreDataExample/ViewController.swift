ca9ea967465917b4e25bb0b9e261e30dd6fc6749
Halil Özel <halilozel1903@gmail.com>
2018-07-03 12:15:20 +0300

    CoreData projesi eklendi.

---
+++ CoreDataExample/CoreDataExample/ViewController.swift
@@ -0,0 +1,124 @@
+//
+//  ViewController.swift
+//  CoreDataExample
+//
+//  Created by Halil Özel on 2.07.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    // verileri diziler yoluyla çekeceğiz
+    var nameArray = [String]()
+    var citynameArray = [String]()
+    var dateArray = [Int]()
+    var imageArray = [UIImage]()
+    var selectedTeams = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        getInfo()
+    }
+    
+    
+    // her sayfa açıldığında çalışan bir metoddur.
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // diğer sayfadan alınan veri ile uyumluysa yenileme işlemi gerçekleşiyor.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.getInfo), name: NSNotification.Name(rawValue : "newTeams"), object: nil)
+    }
+    
+    @objc func getInfo(){
+        
+        // içerisindeki tüm verileri sil.
+        nameArray.removeAll(keepingCapacity: false)
+        citynameArray.removeAll(keepingCapacity: false)
+        dateArray.removeAll(keepingCapacity: false)
+        imageArray.removeAll(keepingCapacity: false)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Teams")  //verileri getir.
+        fetchRequest.returnsObjectsAsFaults = false // verileri dizilere eklemek için false yaptık.
+        
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            
+            if results.count > 0 { // eğer array içerisinde bir şey varsa
+                
+                for result in results as! [NSManagedObject]{ // sonuc değeri NSManagedObject türünde olacak
+                    
+                    if let name = result.value(forKey: "name") as? String{
+                        self.nameArray.append(name)
+                    }
+                    
+                    if let cityName = result.value(forKey: "cityname") as? String{
+                        self.citynameArray.append(cityName)
+                    }
+                    
+                    if let date = result.value(forKey: "date") as? Int{
+                        self.dateArray.append(date)
+                    }
+                    
+                    if let imageData = result.value(forKey: "image") as? Data{
+                        
+                        let image = UIImage(data: imageData)
+                        self.imageArray.append(image!)
+                    }
+                    
+                    self.tableView.reloadData() // tableview guncelle
+                    
+                    
+                }
+            }
+            
+        } catch  {
+            print("ERROR")
+        }
+        
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "detailsVC"{
+            let destinationVC = segue.destination as! detailsVC
+            destinationVC.chosenTeams = selectedTeams // kullanıcının secimini aldık diğer tarafa aktardık.
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+         selectedTeams = nameArray[indexPath.row] // secilen degerlerle gelecek degerin index değerini esitledik.
+         performSegue(withIdentifier: "detailsVC", sender: nil) // segue olusturduk.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count // listede bulunacak satır sayısı
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row] // satırlarda bulunacak degerler
+        return cell
+    }
+
+    @IBAction func addClicked(_ sender: Any) {
+        selectedTeams = "" // yeni birşey secilmediginde bos olarak gösterebiliriz.
+        performSegue(withIdentifier: "detailsVC", sender: nil) // segue olusturduk.
+        
+    }
+    
+}
+
---
+++ CoreDataExample/CoreDataExample/detailsVC.swift
@@ -0,0 +1,143 @@
+//
+//  detailsVC.swift
+//  CoreDataExample
+//
+//  Created by Halil Özel on 2.07.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import UIKit
+
+import CoreData // CoreData'ya ait yapıları kullanmamızı sağlıyor.
+
+class detailsVC: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var cityNameText: UITextField!
+    @IBOutlet weak var dateText: UITextField!
+    
+    var chosenTeams = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if chosenTeams != ""{
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Teams")
+            
+            // name değeri chosenTeams değerine eşitse getir. Filtre uygulandı.
+            fetchRequest.predicate = NSPredicate(format: "name = %@", self.chosenTeams)
+            
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do{
+                
+                let results = try context.fetch(fetchRequest)
+                
+                if results.count > 0 {
+                    
+                    for result in results as! [NSManagedObject]{
+                        
+                        if let name = result.value(forKey: "name") as? String{
+                            
+                            nameText.text = name
+                        }
+                        
+                        if let cityName = result.value(forKey: "cityname") as? String{
+                            
+                            cityNameText.text = cityName
+                        }
+                        
+                        if let date = result.value(forKey: "date") as? Int{
+                            
+                            dateText.text = String(date)
+                        }
+                        
+                        if let dataImage = result.value(forKey: "image") as? Data{
+                            
+                            let image = UIImage(data: dataImage)
+                            self.imageView.image = image
+                        }
+                        
+                    }
+                }
+                
+                
+            }catch{
+                
+                print("Error")
+                
+            }
+        }
+
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(detailsVC.selectedImage))
+        imageView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func selectedImage(){ // resim secme islemi yapılıyor.
+        
+        let picker = UIImagePickerController() // picker nesnesi olusturuldu.
+        picker.delegate = self
+        picker.sourceType = .photoLibrary // daha önceden cekilmis fotoları kutuphaneden alır.
+        picker.allowsEditing = true // resimlere edit işlemi yapsın mı ?
+        present(picker, animated: true, completion: nil) // Bir denetleyici modu sunar.
+        
+    }
+    
+    // image secildikten sonraki işlemler
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imageView.image = info[UIImagePickerControllerEditedImage] as? UIImage //sectiğin yapı resim
+        self.dismiss(animated: true, completion: nil) // sectikten sonra kapat
+    }
+
+
+    @IBAction func saveClicked(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate //app delegate değişkeni olarak tanımladık.
+        let context = appDelegate.persistentContainer.viewContext // context değeri tanımlanıyor.
+        
+        // core data işleminde kullanılacak yapı eşleniyor.
+        let newTeams = NSEntityDescription.insertNewObject(forEntityName: "Teams", into: context)
+        
+        // attributes
+        
+        // string ifadeleri kayıt etme
+        newTeams.setValue(nameText.text, forKey: "name")
+        newTeams.setValue(cityNameText.text, forKey: "cityname")
+        
+        
+        // integer ifadeyi kayıt etme
+        if let year = Int(dateText.text!){
+            
+            newTeams.setValue(year, forKey: "date")
+            
+        }
+        
+        
+        // image ifadeyi kayıt etme
+        let data = UIImageJPEGRepresentation(imageView.image!, 0.5) // ikinci parametre sıkıstırma miktarı
+        newTeams.setValue(data, forKey: "image")
+        
+        do{ // bir hata olursa
+            try context.save() // context değerini kayıt et
+            print("succesful")
+        }catch{ // hatayı bastır
+            print("error")
+        }
+        
+        // burada verilen değeri karşı tarafta eşleyip sayfası yenileme işlemi yapacağız.
+        
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue : "newTeams"), object: nil)
+        
+        self.navigationController?.popViewController(animated: true) // bulunduğun yerden bir öncekine git.
+        
+    }
+    
+
+}
