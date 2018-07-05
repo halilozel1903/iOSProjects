717b24096a1474c5bd988c66992bdefd6b5d4742
Halil Özel <halilozel1903@gmail.com>
2018-07-05 14:49:16 +0300

    TouchID projesi eklendi.

---
+++ TouchID/TouchID/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  TouchID
+//
+//  Created by Halil Özel on 5.07.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+/*
+ 
+ Bir LAContext nesnesi bir kimlik doğrulama bağlamını temsil eder.
+ LAContext sınıfı, kimlik doğrulama ilkelerini ve erişim denetimlerini değerlendirmek,
+ kimlik bilgilerini yönetmek ve kimlik doğrulama bağlamlarını geçersiz kılmak için programlı bir arabirim sağlar.
+ Kimlik doğrulama ilkeleri, kimlik doğrulama ilkelerini değerlendirmek için kullanılır
+ ve uygulamaların kullanıcının biyometrik yöntemleri kullanarak kendilerini doğrulamasını isteme izni verir.
+ 
+ */
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var statusText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        let authContext = LAContext()
+        var error : NSError?
+        
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Gerçekten siz misiniz?") { (success, error) in
+                
+                if success == true{ // işlem doğruysa
+                    
+                    self.statusText.text = "Success" // mesaj yaz
+                    
+                }else{ // işlem yanlışsa
+                    self.statusText.text = "No" // mesaj yaz
+                }
+                
+            }
+            
+        }
+        
+    
+        
+        
+    }
+
+   
+
+
+}
+
