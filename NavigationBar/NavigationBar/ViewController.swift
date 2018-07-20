b4b96397ffe0f27706a54b626b17b5f962758f63
Halil Özel <halilozel1903@gmail.com>
2018-07-20 18:38:46 +0300

    NavigationBar örneği eklendi.

---
+++ NavigationBar/NavigationBar/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  NavigationBar
+//
+//  Created by Halil Özel on 20.07.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    // MARK: - IBAction
+    
+    @IBAction func addClicked(_ sender: Any) {
+        
+        print("Add butonuna tıklandı.")
+    }
+    
+    
+    @IBAction func cameraClicked(_ sender: Any) {
+        
+        print("Camera'ya tıklandı")
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+
+}
+
