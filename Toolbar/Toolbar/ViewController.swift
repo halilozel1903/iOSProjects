d52108246b6452ab3bdc3298d701c419abcd0bd1
Halil Özel <halilozel1903@gmail.com>
2018-07-20 18:39:36 +0300

    Toolbar örneği eklendi.

---
+++ Toolbar/Toolbar/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Toolbar
+//
+//  Created by Halil Özel on 20.07.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - IBAction
+    
+    @IBAction func loadButton(_ sender: Any) {
+        print("load butonuna basıldı.")
+    }
+    
+
+    @IBAction func stopButton(_ sender: Any) {
+        
+        print("stop butonuna basıldı.")
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+}
+
