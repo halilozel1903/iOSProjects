320350fe0a64149b213113159b75993432fc59b2
Halil Özel <halilozel1903@gmail.com>
2018-07-23 13:10:58 +0300

    Coklu View Controller projesi eklendi.

---
+++ ViewControllerApp/ViewControllerApp/IkinciViewController.swift
@@ -0,0 +1,35 @@
+//
+//  IkinciViewController.swift
+//  ViewControllerApp
+//
+//  Created by Halil Özel on 23.07.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import UIKit
+
+class IkinciViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
