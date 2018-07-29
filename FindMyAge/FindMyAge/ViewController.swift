e03e55d421b9780aaaf00665217fbee223ab71e1
Halil Özel <halilozel1903@gmail.com>
2018-07-29 16:18:13 +0300

    FindMyAge ornegi eklendi.

---
+++ FindMyAge/FindMyAge/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  FindMyAge
+//
+//  Created by Halil Özel on 29.07.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITextFieldDelegate {
+
+    
+    @IBOutlet weak var txtYearOfBirth: UITextField!
+    @IBOutlet weak var labelShowAge: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+   
+    @IBAction func btnFindAge(_ sender: Any) {
+        
+        let yearOfBirth = Int (txtYearOfBirth.text!)
+        
+        // tarih değerlerini alıyoruz.
+        let date = Date()
+        let calendar = Calendar.current
+        let year = calendar.component(.year, from: date)
+        
+        // tarih değerinden doğum yılını çıkartıp sonucu buluyoruz.
+        let personAge = year - yearOfBirth!
+        
+        // let personAge = 2018 - yearOfBirth!
+        
+        // ekranda gösteriyoruz.
+        labelShowAge.text = "Your age is \(personAge) years old"
+    }
+    
+    // ekranda herhangibir alana basılınca klavye kapanır.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
+    
+    
+    // return tuşuna basınca klavye kapanacak.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    
+        self.view.endEditing(true)
+        return true
+    }
+    
+    
+}
+
