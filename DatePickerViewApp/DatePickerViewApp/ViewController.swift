91a93a227a9e9c943f401cd7d70cc41237a704b4
Halil Özel <halilozel1903@gmail.com>
2018-08-03 10:47:53 +0300

    DatePickerViewApp dersi eklendi.

---
+++ DatePickerViewApp/DatePickerViewApp/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  DatePickerViewApp
+//
+//  Created by Halil Özel on 3.08.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var datePickerView: UITextField!
+    
+    // date picker nesnesi
+    private var datePicker : UIDatePicker?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // datepicker işlemleri
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .date
+        datePicker?.addTarget(self, action: #selector(ViewController.dataChanged(dataPicker:)), for: .valueChanged)
+        
+        // dokunma nesnesi oluşturuldu.
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.viewTapped(gestureRecognizer:)))
+        
+        view.addGestureRecognizer(tapGesture)
+        
+        // input değerini tarih değerine atadı.
+        datePickerView.inputView = datePicker
+    
+        
+        
+    }
+    
+    // ekrana bir yere tıklayınca dataPicker kapanacak.
+    @objc func viewTapped(gestureRecognizer:UITapGestureRecognizer){
+        view.endEditing(true)
+    }
+    
+    // tarihi değiştirmek için kullanılan metod
+    @objc func dataChanged(dataPicker: UIDatePicker){
+        
+        // formatlama işlemleri
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        datePickerView.text = dateFormatter.string(from: (datePicker?.date)!)
+        view.endEditing(true)
+        
+    }
+
+    
+
+
+}
+
