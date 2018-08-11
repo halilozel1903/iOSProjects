2b014cf63abdcdc40f1581acb944474e50a0577f
Halil Özel <halilozel1903@gmail.com>
2018-08-11 22:21:56 +0300

    SpeechRecognition örneği eklendi.

---
+++ SpeechRecognition/SpeechRecognition/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  SpeechRecognition
+//
+//  Created by Halil Özel on 10.08.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import Speech
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var txtMusic: UITextView!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    var audioPlayer : AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    @IBAction func playClicked(_ sender: Any) {
+        
+        self.requestSpeechAuth()
+    }
+    
+    func requestSpeechAuth(){
+        
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in
+            
+            if authStatus == SFSpeechRecognizerAuthorizationStatus.authorized{
+                
+                if let path = Bundle.main.url(forResource: "music", withExtension: "mp3"){
+                    
+                    do{
+                        let audio = try AVAudioPlayer(contentsOf: path)
+                        self.audioPlayer = audio
+                        self.audioPlayer.play()
+                        
+                    }catch{
+                    
+                        print("Error !!!")
+                    }
+                    
+                    let recognizer = SFSpeechRecognizer()
+                    let request = SFSpeechURLRecognitionRequest(url: path)
+                    
+                    recognizer?.recognitionTask(with: request, resultHandler: { (result, error) in
+                        if let err = error{
+                            print("error : \(err)")
+                        }else{
+                            self.txtMusic.text = result?.bestTranscription.formattedString
+                        }
+                    })
+                }
+                
+            }
+            
+        }
+    }
+    
+
+}
+
