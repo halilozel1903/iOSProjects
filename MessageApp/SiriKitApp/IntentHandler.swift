01d239453b93a918a827bb81d05c0fdb4ef02f8d
Halil Özel <halilozel1903@gmail.com>
2018-08-13 12:02:33 +0300

    SiriKit uygulaması eklendi.

---
+++ MessageApp/SiriKitApp/IntentHandler.swift
@@ -0,0 +1,73 @@
+//
+//  IntentHandler.swift
+//  SiriKitApp
+//
+//  Created by Halil Özel on 13.08.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import Intents
+
+// As an example, this class is set up to handle Message intents.
+// You will want to replace this or add other intents as appropriate.
+// The intents you wish to handle must be declared in the extension's Info.plist.
+
+// You can test your example integration by saying things to Siri like:
+// "Send a message using <myApp>"
+// "<myApp> John saying hello"
+// "Search for messages in <myApp>"
+
+class IntentHandler: INExtension, INSendMessageIntentHandling {
+    
+    // siriye söylenen mesaj ile ilgili işlemlerin yapıldığı alandır.
+    func resolveContent(for intent: INSendMessageIntent, with completion: @escaping (INStringResolutionResult) -> Void) {
+        
+        if let content = intent.content{
+            
+            print("gelen bilgi : \(content)")
+            
+            let response = INStringResolutionResult.success(with: content)
+            completion(response)
+        }else{
+            let response = INStringResolutionResult.needsValue()
+            completion(response)
+        }
+        
+    }
+    
+    // hesaplı işlemlerde giriş yapılıp yapılmadığını öğrenilen alandır.
+    func confirm(intent: INSendMessageIntent, completion: @escaping (INSendMessageIntentResponse) -> Void) {
+        
+        var  isLogged = true
+        
+        // başarılı ise
+        if isLogged{
+            
+            completion(INSendMessageIntentResponse(code: .success, userActivity: nil))
+            
+        }
+            
+            // başarısız ise 
+        else{
+            let response = INSendMessageIntentResponse(code: .failureRequiringAppLaunch, userActivity: nil)
+            completion(response)
+        }
+        
+    }
+    
+    // kullanıcıdan gerekli izinler alındıktan sonra mesaj yollanılır.
+    func handle(intent: INSendMessageIntent, completion: @escaping (INSendMessageIntentResponse) -> Void) {
+        
+    }
+    
+    
+    
+    override func handler(for intent: INIntent) -> Any {
+        // This is the default implementation.  If you want different objects to handle different intents,
+        // you can override this and return the handler you want for that particular intent.
+        
+        return self
+    }
+    
+}
+
---
+++ MessageApp/SiriKitAppUI/IntentViewController.swift
@@ -0,0 +1,42 @@
+//
+//  IntentViewController.swift
+//  SiriKitAppUI
+//
+//  Created by Halil Özel on 13.08.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import IntentsUI
+
+// As an example, this extension's Info.plist has been configured to handle interactions for INSendMessageIntent.
+// You will want to replace this or add other intents as appropriate.
+// The intents whose interactions you wish to handle must be declared in the extension's Info.plist.
+
+// You can test this example integration by saying things to Siri like:
+// "Send a message using <myApp>"
+
+class IntentViewController: UIViewController, INUIHostedViewControlling {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - INUIHostedViewControlling
+    
+    // Prepare your view controller for the interaction to handle.
+    func configureView(for parameters: Set<INParameter>, of interaction: INInteraction, interactiveBehavior: INUIInteractiveBehavior, context: INUIHostedViewContext, completion: @escaping (Bool, Set<INParameter>, CGSize) -> Void) {
+        // Do configuration here, including preparing views and calculating a desired size for presentation.
+        completion(true, parameters, self.desiredSize)
+    }
+    
+    var desiredSize: CGSize {
+        return self.extensionContext!.hostedViewMaximumAllowedSize
+    }
+    
+}
