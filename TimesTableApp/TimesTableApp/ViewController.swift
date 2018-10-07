3dcdfb41d6cd81bb1cb480d89a782d1c2686cf16
Halil Özel <halilozel1903@gmail.com>
2018-10-07 23:14:40 +0300

    TimesTableApp adlı örnek eklendi.

---
+++ TimesTableApp/TimesTableApp/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  TimesTableApp
+//
+//  Created by Halil Özel on 7.10.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    // slider değişkeni
+    @IBOutlet weak var slider: UISlider!
+    
+    // tableView değişkeni
+    @IBOutlet weak var table: UITableView!
+    
+    // slider metodu
+    @IBAction func sliderChanged(_ sender: Any) {
+        table.reloadData() // değerleri güncelle
+        // print(slider.value)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50 // satır sayısı
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // cell tanımı
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        
+        // işlemler
+        cell.textLabel?.text = String((Int(slider.value * 20)) * (indexPath.row + 1))
+        
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+    
+
+}
+
