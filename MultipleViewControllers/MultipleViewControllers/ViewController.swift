7238d0fa489af098619aff79295a9ef37945df74
Halil Özel <halilozel1903@gmail.com>
2018-10-08 09:40:39 +0300

    MultipleViewControllers ornegi eklendi.

---
+++ MultipleViewControllers/MultipleViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultipleViewControllers
+//
+//  Created by Halil Özel on 8.10.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
