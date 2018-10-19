0cf5d8d1cf4a86a793a3199d0b43c9b8bba9ea8f
Halil Özel <halilozel1903@gmail.com>
2018-10-19 20:53:36 +0300

    AutoLayoutExample adlı örnek eklendi.

---
+++ AutoLayoutExample/AutoLayoutExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutExample
+//
+//  Created by Halil Özel on 19.10.2018.
+//  Copyright © 2018 Halil Özel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
