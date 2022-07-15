30c81f7d7e0106a9ccadd2ef8bdf274099725fd3
Halil Özel <halilozel1903@gmail.com>
2022-07-15 15:16:14 +0300

    AutoLayoutExample was updated.

--- AutoLayoutExample/AutoLayoutExample/ViewController.swift
+++ AutoLayoutExample/AutoLayoutExample/ViewController.swift
@@ -2,19 +2,17 @@
 //  ViewController.swift
 //  AutoLayoutExample
 //
-//  Created by Halil Özel on 19.10.2018.
+//  Created by Halil Özel on 15.07.2022.
 //  Copyright © 2018 Halil Özel. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
