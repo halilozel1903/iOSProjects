8ed9887d02b62b7d02ceb2aa9ed458e71c16dcbe
Halil Özel <halilozel1903@gmail.com>
2022-07-15 14:13:05 +0300

    ViewControllerApp was refactored.

--- ViewControllerApp/ViewControllerApp/ViewController.swift
+++ ViewControllerApp/ViewControllerApp/ViewController.swift
@@ -2,24 +2,22 @@
 //  ViewController.swift
 //  ViewControllerApp
 //
-//  Created by Halil Özel on 23.07.2018.
+//  Refactored by Halil Özel on 15.07.2022.
 //  Copyright © 2018 Halil Özel. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
