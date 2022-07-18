f76cf15ae1ed8d77e1574f0400c0e7489560e68a
Halil Özel <halilozel1903@gmail.com>
2022-07-19 00:19:35 +0300

    SwitchScreens was updated.

--- SwitchScreens/SwitchScreens/ThirdViewController.swift
+++ SwitchScreens/SwitchScreens/ThirdViewController.swift
@@ -2,16 +2,13 @@
 //  ThirdViewController.swift
 //  SwitchScreens
 //
-//  Created by Halil Özel on 27.07.2018.
+//  Updated by Halil Özel on 19.07.2022.
 //  Copyright © 2018 Halil Özel. All rights reserved.
 //
 
 import UIKit
 
 class ThirdViewController: UIViewController {
-
-    
-    
     
     @IBAction func closeAction(_ sender: Any) {
         
@@ -21,24 +18,24 @@ class ThirdViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- SwitchScreens/SwitchScreens/ViewController.swift
+++ SwitchScreens/SwitchScreens/ViewController.swift
@@ -2,43 +2,33 @@
 //  ViewController.swift
 //  SwitchScreens
 //
-//  Created by Halil Özel on 27.07.2018.
+//  Updated by Halil Özel on 19.07.2022.
 //  Copyright © 2018 Halil Özel. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
     
     @IBAction func ikinciEkranaGit(_ sender: Any) {
-        
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "SecondStoryboardID")
         self.show(vc!, sender: nil)
-        
     }
     
-    
-    
     @IBAction func ucuncuEkranaGit(_ sender: Any) {
-        
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "ThirdStoryboardID")
         
         self.present(vc!, animated: true, completion: nil)
     }
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
