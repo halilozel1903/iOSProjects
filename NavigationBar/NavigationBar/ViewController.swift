b2146cb9dee5c22118cef0ae826decef9642c0ec
Halil Özel <halilozel1903@gmail.com>
2022-07-18 11:38:19 +0300

    NavigationBar was refactored.

--- NavigationBar/NavigationBar/ViewController.swift
+++ NavigationBar/NavigationBar/ViewController.swift
@@ -2,26 +2,26 @@
 //  ViewController.swift
 //  NavigationBar
 //
-//  Created by Halil Özel on 20.07.2018.
+//  Refactored by Halil Özel on 18.07.2022.
 //  Copyright © 2018 Halil Özel. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    
     // MARK: - IBAction
     
     @IBAction func addClicked(_ sender: Any) {
-        
-        print("Add butonuna tıklandı.")
+        let alert = UIAlertController(title: "Info", message: "Add butonuna tıklandı.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true)
     }
     
     
     @IBAction func cameraClicked(_ sender: Any) {
-        
-        print("Camera'ya tıklandı")
+        let alert = UIAlertController(title: "Info", message: "Camera butonuna tıklandı.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true)
     }
     
     
@@ -29,9 +29,5 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
-
-
 }
 
