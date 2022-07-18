246867734e5a97c609df5bf26d785b75496bbceb
Halil Özel <halilozel1903@gmail.com>
2022-07-18 11:23:57 +0300

    QuickActionApp was refactored.

--- QuickActionApp/QuickActionApp/ViewController.swift
+++ QuickActionApp/QuickActionApp/ViewController.swift
@@ -2,24 +2,22 @@
 //  ViewController.swift
 //  QuickActionApp
 //
-//  Created by Halil Özel on 15.08.2018.
+//  Refactored by Halil Özel on 18.07.2022.
 //  Copyright © 2018 Halil Özel. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
