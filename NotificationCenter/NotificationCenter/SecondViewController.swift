ea0fa0f109eb8490e4ab1947d19a79cc70ec953a
Halil Özel <halilozel1903@gmail.com>
2022-07-18 11:28:07 +0300

    NotificationCenter was refactored.

--- NotificationCenter/NotificationCenter/SecondViewController.swift
+++ NotificationCenter/NotificationCenter/SecondViewController.swift
@@ -2,34 +2,26 @@
 //  SecondViewController.swift
 //  NotificationCenter
 //
-//  Created by Halil Özel on 28.07.2018.
+//  Refactored by Halil Özel on 18.07.2022.
 //  Copyright © 2018 Halil Özel. All rights reserved.
 //
 
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
-    
-    
-
     
     @IBAction func teamSelectAction(_ sender: UIButton) {
         
         let dict : [String : String] = ["name": sender.title(for: .normal)!]
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: "teamSelectNotification"), object: nil, userInfo: dict)
         
-
         self.dismiss(animated: true, completion: nil)
         
     }
-    
-   
-
 }
--- NotificationCenter/NotificationCenter/ViewController.swift
+++ NotificationCenter/NotificationCenter/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var teamLabel: UILabel!
     
@@ -18,30 +18,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         NotificationCenter.default.addObserver(self, selector: #selector(self.changeName(_:)), name: NSNotification.Name(rawValue: "teamSelectNotification"), object: nil)
-        
     }
-
     
-
+    
+    
     @IBAction func teamSelected(_ sender: Any) {
-        
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        
         let vc = storyboard.instantiateViewController(withIdentifier: "StoryboardViewControllerID") as! SecondViewController
-        
         self.present(vc, animated: true, completion: nil)
     }
     
     @objc func changeName(_ notification : NSNotification){
-        
         if let name = notification.userInfo?["name"] as? String{
-            
             self.teamLabel.text = name
-            
         }
-        
-        
     }
-    
 }
 
